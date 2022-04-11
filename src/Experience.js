6ba1ad4a52120d8d4fd1b93326859aaeab2fdbb2
Yasir <code.yasir@gmail.com>
2022-04-11 05:09:27 +0300

    add experience component

---
+++ src/Experience.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Experience() {
+  return (
+    <article className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+    </article>
+  );
+}
+
+export default Experience;
