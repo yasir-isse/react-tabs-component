047f6e13fc9354afc11d79d11cf37e28664de523
Yasir <code.yasir@gmail.com>
2022-04-11 05:10:37 +0300

    add app logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+import React, { useState, useEffect } from "react";
+import Job from "./Job";
+import JobsList from "./JobsList";
+import Loading from "./Loading";
+import Experience from "./Experience";
+
+const url = "https://course-api.com/react-tabs-project";
+
 function App() {
-  return <h2>tabs project setup</h2>
+  const [experience, setExperience] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [value, setValue] = useState(0);
+
+  const changeJob = (index) => {
+    setValue(index);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setExperience(data);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        return console.error(error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <section className="section">
+      <Experience />
+      <div className="jobs-center">
+        <div className="btn-container">
+          {experience.map((item, index) => {
+            return (
+              <JobsList
+                key={item.id}
+                name={item.company}
+                changeJob={changeJob}
+                index={index}
+                jobs={experience}
+                value={value}
+              />
+            );
+          })}
+        </div>
+        <Job {...experience[value]} />
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
